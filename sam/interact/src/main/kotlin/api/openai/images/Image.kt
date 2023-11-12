ef1b3e39f3ec4e1e30eae240e33862ec91d7e01c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-12 23:35:25 +0000

    Fix DALL-E 3 API

--- sam/interact/src/main/kotlin/api/openai/images/Image.kt
+++ sam/interact/src/main/kotlin/api/openai/images/Image.kt
@@ -3,4 +3,8 @@ package cloud.drakon.tempestbot.interact.api.openai.images
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable class ImageB64Json(@SerialName("b64_json") val b64Json: String)
+@Serializable
+class Image(
+    @SerialName("b64_json") val b64Json: String,
+    @SerialName("revised_prompt") val revisedPrompt: String,
+)
--- sam/interact/src/main/kotlin/api/openai/images/ImageResponse.kt
+++ sam/interact/src/main/kotlin/api/openai/images/ImageResponse.kt
@@ -4,5 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable class ImageResponse(
     val created: Int,
-    val data: Array<ImageB64Json>,
+    val data: Array<Image>,
 )
