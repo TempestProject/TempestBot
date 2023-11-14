b6b8ec2447eee2008d534fb98ea4f5a32516f841
Adam Chance <6444703+drakon64@users.noreply.github.com>
2023-11-14 22:17:36 +0000

    Bump dependencies

--- sam/interact/build.gradle.kts
+++ sam/interact/build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    val kotlinVersion = "1.9.20"
+
+    kotlin("jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
 }
 
 group = "cloud.drakon"
@@ -41,24 +43,24 @@ repositories {
 }
 
 dependencies {
-    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.3")
 
     implementation("cloud.drakon:ktdiscord:6.1.0")
 
-    val ktorVersion = "2.3.2"
+    val ktorVersion = "2.3.6"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-java:$ktorVersion")
     implementation("io.ktor:ktor-client-auth:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
-    implementation("org.mongodb:mongodb-driver-sync:4.10.1")
+    implementation("org.mongodb:mongodb-driver-sync:4.11.1")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     // Translate
-    implementation("aws.sdk.kotlin:translate:0.28.1-beta")
+    implementation("aws.sdk.kotlin:translate:0.34.7-beta")
 
     testImplementation(kotlin("test"))
 }
